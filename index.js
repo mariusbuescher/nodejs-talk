36ba339efe6f69df71a6e03430e4846ca2b0de2d
Marius Büscher <marius.buescher@denkwerk.com>
2015-12-08 15:14:28 +0100

    Dependencies sample

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var fs = require( 'fs' );
+var chalk = require( 'chalk' );
 
 fs.readFile( 'message.txt', 'utf8', function( err, msg ) {
     if ( err ) {
         throw err;
     }
 
-    console.log( msg );
+    console.log( chalk.bold.red( msg ) );
 } );
