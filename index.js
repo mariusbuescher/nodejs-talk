096bd0dd5ee4a4149447c8c7ec1f357274357f9e
Marius Büscher <marius.buescher@denkwerk.com>
2015-12-08 15:11:11 +0100

    Async sample

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-var msg = require( './message.js' );
-console.log( msg );
+var fs = require( 'fs' );
+
+fs.readFile( 'message.txt', 'utf8', function( err, msg ) {
+    if ( err ) {
+        throw err;
+    }
+
+    console.log( msg );
+} );
